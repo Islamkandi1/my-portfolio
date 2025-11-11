27f0e99f337c0ad616b1ac86798a66e2b6dd4958
Islamkandi1 <eslamkandil097@gmail.com>
2025-11-11 20:22:52 +0200

    handle open project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,33 +1,32 @@
 import { ExternalLink } from "lucide-react";
 import React from "react";
-import images from './../../assets/images/images';
+import images from "./../../assets/images/images";
 const Projects = () => {
-    // my project===============================
+  // my project===============================
+  console.log(innerWidth >= 767);
 
   const projects = [
     {
       title: "E-Commerce Platform",
       description:
         "Built a modern e-commerce web app with Next.js, Tailwind CSS, typescript and shadcn/ui, delivering a   clean, responsive, and user-friendly interface across all devices",
-      image:
-        images.Ecommerce,
-      tags: ["Next", "Typescript", "tailwind", "Shadcn","Axios"],
+      image: images.Ecommerce,
+      tags: ["Next", "Typescript", "tailwind", "Shadcn", "Axios"],
       link: "https://fresh-cart-liart-kappa.vercel.app/",
     },
     {
       title: "Facebook Clone",
       description:
         "Responsive design using Tailwind CSS and DaisyUI, developed with React.js. Includes Dark/Light mode that automatically adapts to the userʼs system preferences or can be manually switched, with persistence using localStorage.",
-      image:
-        images.facebook,
-      tags: ["React", "tailwind", "daisyui"," Tanstack Query","Axios"],
+      image: images.facebook,
+      tags: ["React", "tailwind", "daisyui", " Tanstack Query", "Axios"],
       link: "https://face-book-social-app.vercel.app/",
     },
     {
       title: "Furni",
       description:
         "Developed an e-commerce app with html ,css and Bootstrap as a framework for designnig, and FontAwesome for a responsive and visually appealing user interface",
-      image:images.furni,
+      image: images.furni,
       tags: ["html", "css", "bootstrap"],
       link: " https://islamkandi1.github.io/furni/",
     },
@@ -35,15 +34,15 @@ const Projects = () => {
       title: "Netflix Clone",
       description:
         " Built with React.js, Tailwind CSS, and MUI. Fully responsive with modern UI components and cinematic design, Axios for fetching movie/series data and TanStack  Query for caching",
-      image:images.Netfiex,
-      tags: ["React", "Tailwind", "Mui","Tanstack Query","Axios"],
+      image: images.Netfiex,
+      tags: ["React", "Tailwind", "Mui", "Tanstack Query", "Axios"],
       link: "https://movies-series-three.vercel.app/",
     },
     {
       title: "Plant store",
       description:
         "A responsive plant store using Html, css and Bootstrap , It showcases a modern layout with smooth navigation and a professional look. ",
-      image:images.plant,
+      image: images.plant,
       tags: ["Html5", "Css3", "Bootstrap"],
       link: "https://islamkandi1.github.io/solplant/",
     },
@@ -51,7 +50,7 @@ const Projects = () => {
       title: "Personal Page",
       description:
         "Built a modern and fully responsive website using HTML, CSS, and Bootstrap, featuring a clean and elegant design optimized for all devices.",
-      image:images.bezel,
+      image: images.bezel,
       tags: ["html", "css", "bootstrap"],
       link: "https://islamkandi1.github.io/bezel/",
     },
@@ -70,17 +69,19 @@ const Projects = () => {
 
           <section className="grid sm:grid-cols-2 lg:grid-cols-3  gap-8">
             {projects.map((project, index) => (
-              <section
+              <a
+                href={project.link}
                 key={index}
+                 target="_blank"
                 className="group bg-white dark:bg-gray-800 rounded-2xl overflow-hidden shadow-xl hover:shadow-2xl transition-all duration-500 transform hover:-translate-y-3"
               >
-                <section className="relative overflow-hidden h-56">
+                <div className="relative overflow-hidden h-56">
                   <img
                     src={project.image}
                     alt={project.title}
                     className="w-full h-full object-cover transform group-hover:scale-125 transition-transform duration-700"
                   />
-                  <section className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex items-end justify-center pb-4">
+                  <div className="absolute inset-0 bg-linear-to-t from-black/80 via-black/40 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500 flex items-end justify-center pb-4">
                     <a
                       href={project.link}
                       target="_blank"
@@ -89,8 +90,8 @@ const Projects = () => {
                       <span>View Project</span>
                       <ExternalLink size={16} />
                     </a>
-                  </section>
-                </section>
+                  </div>
+                </div>
 
                 <section className="p-6">
                   <h3 className="text-2xl dark:text-white font-bold mb-3 group-hover:text-blue-500 transition-colors">
@@ -99,7 +100,7 @@ const Projects = () => {
                   <p className="text-gray-600 dark:text-gray-400 mb-4 line-clamp-2">
                     {project.description}
                   </p>
-                  <div className="flex flex-wrap gap-2">
+                  <section className="flex flex-wrap gap-2">
                     {project.tags.map((tag, i) => (
                       <span
                         key={i}
@@ -108,9 +109,9 @@ const Projects = () => {
                         {tag}
                       </span>
                     ))}
-                  </div>
+                  </section>
                 </section>
-              </section>
+              </a>
             ))}
           </section>
         </section>
